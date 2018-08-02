9b3878fa878501e5ad22ef9a88eae6ecdfa8ce30
Brittany Tinnin <bntinnin7@gmail.com>
2018-08-02 21:31:10 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -78,8 +78,12 @@ class Student
   end
 
 
-  def self.all_students_in_grade_X
-    
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE grade = ?
+    SQL
+    DB[:conn].execute(sql, x).map { |row| self.new_from_db(row)  }
   end
 
   def save
