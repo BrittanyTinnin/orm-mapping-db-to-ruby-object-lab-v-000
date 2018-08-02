3bead82a29fc8980359679bb98c0dc62ac0d906d
Brittany Tinnin <bntinnin7@gmail.com>
2018-08-02 21:26:49 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,20 +32,9 @@ class Student
     WHERE name = ?
     LIMIT 1
     SQL
-
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
     end.first
-  end
-
-  def self.count_all_students_in_grade_9
-    sql = <<-SQL
-      SELECT * FROM students
-      WHERE grade = 9
-      SQL
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end
   end
 
 
